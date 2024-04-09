771e10dbec052fddcd886035de53ddc40886dea1
Leiliane Nascimento <121403224+leilianenascimento@users.noreply.github.com>
2024-04-09 18:22:46 -0300

    Add files via upload
    Criei arquivo dados.php para ser realizado a modificação de saldo.

---
+++ dados.php
@@ -0,0 +1,9 @@
+<?php 
+$dadospessoais = [
+    ['nome' => 'Maria', 'idade' => 30, 'saldo'=> 500],
+    ['nome' => 'Joao', 'idade' =>20, 'saldo' => 200],
+    ['nome' => 'Aline', 'idade' =>20, 'saldo' => 2000],
+    ['nome' => 'Leila', 'idade' =>25, 'saldo' => 1000],
+    
+    ];
+    
